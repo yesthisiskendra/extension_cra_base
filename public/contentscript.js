25ce4a1e4dca00b392ca458375846a910355836b
Kendra Ryan <yesthisiskendra@gmail.com>
2020-06-27 13:53:29 -0700

    Adding Bill Murray

--- public/contentscript.js
+++ public/contentscript.js
@@ -1,10 +1,15 @@
 /* global chrome */
 chrome.runtime.onMessage.addListener((request) => {
-  if (request.animal) {
+  if (request.subject) {
     let images = Array.from(document.querySelectorAll("img"));
     for (const image of images) {
-      image.src =
-        "http://placekitten.com/g/" + image.width + "/" + image.height;
+      if (request.subject === "Bill Murray") {
+        image.src =
+          "http://www.fillmurray.com/" + image.width + "/" + image.height;
+      } else {
+        image.src =
+          "http://placekitten.com/" + image.width + "/" + image.height;
+      }
     }
   }
 });
--- src/App.js
+++ src/App.js
@@ -1,16 +1,43 @@
 /* global chrome */
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 
 function App() {
-  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-    let currentTab = tabs[0].id;
-    chrome.tabs.sendMessage(currentTab, { animal: "kitten" });
-  });
+  const [selection, setSelection] = useState("kittens");
 
+  const handleClick = () => {
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+      let currentTab = tabs[0].id;
+      chrome.tabs.sendMessage(currentTab, { subject: selection });
+    });
+  };
   return (
     <div className="App">
-      <h1>MAKE THEM ALL CATS!</h1>
+      <h1>Ew! I don't like these images.</h1>
+      <h3>I'd much ather look at:</h3>
+      <form>
+        <input
+          type="radio"
+          id="k"
+          value="kittens"
+          checked={selection === "kittens"}
+          onClick={() => setSelection("kittens")}
+        />
+        <label for="k">Kittens</label>
+
+        <input
+          type="radio"
+          id="bm"
+          value="Bill Murray"
+          checked={selection === "Bill Murray"}
+          onClick={() => setSelection("Bill Murray")}
+        />
+        <label for="bm">Bill Murray</label>
+      </form>
+
+      <div class="btn green" onClick={handleClick}>
+        Make all images {selection}!
+      </div>
     </div>
   );
 }
